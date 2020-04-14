8c73050a6c20dfaaf476e9ef34b3a9b4004776c3
Carol Meng <meng1932@gmail.com>
2020-04-14 17:25:30 -0400

    init

---
+++ CORD2020/.umirc.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  routes: [
+    { path: '/', component: '@/pages/index' },
+    { path: '/answer', component: '@/pages/result' },
+  ],
+});
---
+++ CORD2020/src/pages/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './index.less';
+
+import { Input } from 'antd';
+
+import titleCover from '../assets/title-cover.png';
+
+const { Search } = Input;
+
+export default () => {
+  return (
+    <div className="empty-search-bar-page">
+      <div className="title-cover-box">
+        <img className="title-cover" src={titleCover} alt="title cover" />
+      </div>
+      <div className="title-box">
+        <h1>COVID-19 Open Research Dataset Challenge (CORD-19)</h1>
+        <h2>
+          An AI challenge with AI2, CZI, MSR, Georgetown, NIH &amp; The White
+          House
+        </h2>
+        <Search
+          placeholder="Ask your question here!"
+          enterButton="Search"
+          size="large"
+          onSearch={value => console.log(value)}
+        />
+      </div>
+    </div>
+  );
+};
